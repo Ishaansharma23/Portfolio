9e7e8998a31e1bc1afabd7a6b972e7992b972810
Ishaansharma23 <sharmaishaan0009@gmail.com>
2024-09-27 22:00:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.text', {
+    strings : ['Frontend Developer', 'Sports Enthusiast', 'Aspiring Tech Innovator'],
+    typeSpeed: 100,
+    backspeed:100,
+    delayspeed:1000,
+});
